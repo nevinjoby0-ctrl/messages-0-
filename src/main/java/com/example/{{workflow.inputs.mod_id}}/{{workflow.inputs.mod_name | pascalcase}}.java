de1ad2e27c638fad61376761dd6f21db1db8dfea
nevinjoby0-ctrl <nevinjoby0@gmail.com>
2025-12-09 11:20:22 +0530

    feat: Add Java source file

---
+++ src/main/java/com/example/{{workflow.inputs.mod_id}}/{{workflow.inputs.mod_name | pascalcase}}.java
@@ -0,0 +1,21 @@
+package com.example.{{workflow.inputs.mod_id}};
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class {{workflow.inputs.mod_name | pascalcase}} implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger("{{workflow.inputs.mod_id}}");
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
